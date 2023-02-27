fae9e9a1083a64a62c6ec5153a48ad41fea017fd
Rdy <1972245397@qq.com>
2023-02-27 13:23:50 +0800

    安装vuex与axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -13,5 +14,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 
 app.use(ElementPlus)
     .use(router)
+    .use(store)
 
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,17 @@
 import { createRouter,createWebHashHistory } from 'vue-router'
 
-// 1. 定义路由组件.
-// 也可以从其他文件导入
 import Home from '../views/Home.vue'
 import NotFound from '../views/NotFound.vue'
 
-// 2. 定义一些路由
-// 每个路由都需要映射到一个组件。
-// 我们后面再讨论嵌套路由。
 const routes = [
   { path: '/', component: Home },
   {path: '/:pathMatch(.*)',component: NotFound}
 ]
 
-// 3. 创建路由实例并传递 `routes` 配置
-// 你可以在这里输入更多的配置，但我们在这里
-// 暂时保持简单
+
 const router = createRouter({
-  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
   history: createWebHashHistory(),
-  routes, // `routes: routes` 的缩写
+  routes, 
 })
 
 export default router
