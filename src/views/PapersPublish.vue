4218d14b3bfaa9a87c9449e53728b1587688c04b
Rdy <1972245397@qq.com>
2023-03-19 10:33:12 +0800

    update

---
+++ src/views/PapersPublish.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <el-table :data="state.singleChoices">
+            <el-table-column type="index" label="题号" width="70">
+            </el-table-column>
+            <el-table-column prop="body" label="题目">
+            </el-table-column>
+
+        </el-table>
+    </div>
+</template>
+
+<script setup>
+import { useStore } from 'vuex';
+import { reactive } from 'vue';
+
+const state = reactive({
+    singleChoices: [],
+})
+
+const store = useStore()
+state.singleChoices = store.state.singleChoices
+console.log(state.singleChoices)
+
+</script>
+
+<style lang="scss" scoped></style>
